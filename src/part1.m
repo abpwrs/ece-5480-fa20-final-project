2324ea7765b228984784561457b60ce9e5784748
Mikayla Biggs <mikayla-biggs@uiowa.edu>
2020-11-17 20:53:51 -0600

    Classification image, class labels, kmeans clustering

--- src/part1.m
+++ src/part1.m
@@ -9,13 +9,15 @@ I = imread(strcat(data_folder, 'PandD.tif'));
 
 %% median filtering
 % this takes awhile
-R = I(:, :, 1); 
-R = rmfilter(R);
-G = I(:, :, 2); 
-G = rmfilter(G);
-B = I(:, :, 3); 
-B = rmfilter(B);
-I_median = cat(3, R, G, B);
+% R = I(:, :, 1); 
+% R = rmfilter(R);
+% G = I(:, :, 2); 
+% G = rmfilter(G);
+% B = I(:, :, 3); 
+% B = rmfilter(B);
+% I_median = cat(3, R, G, B);
+
+I_median = imread(strcat(data_folder, 'PandD_rmfilter.png'));
 
 %% morphological processing
 morph_data = false(size(I_median));
@@ -80,10 +82,10 @@ I_masked = I .* uint8(BW_coin_mask);
 figure(4); imshow(I_masked,[]);
 I_masked_eq = histeq(I_masked);
 figure(5);imshow(I_masked_eq,[]);
-[L, centers] = imsegkmeans(I_masked, 3);
+%[L, centers] = imsegkmeans(I_masked, 3);
 
 %%
-figure(5);
+figure(6);
 for channel=1:3 
     subplot(3,1,channel)
     I_channel = double(I_masked(:,:,channel));
@@ -92,17 +94,49 @@ for channel=1:3
 end
 %% classification label overlay
 class_labeloverlay = labeloverlay(I, L);
-figure(5);imshow(class_labeloverlay,[]);
+figure(7);imshow(class_labeloverlay,[]);
 
 %% multi thresh
 
 thresh = multithresh(I_masked,2);
 seg_I = imquantize(I_masked,thresh);
 % RGB = label2rgb(seg_I); 	 
-figure(1);imshow(seg_I,[]);
+figure(8);imshow(seg_I,[]);
 
 %% radii based classification
+thresh = 48.0;
 
+thresh_labels =  kmeans(radii,2);
 
+class_info = cat(2, thresh_labels, radii);
+class_1 = radii(thresh_labels==1);
+class_2 = radii(thresh_labels==2);
 
+mu_1 = mean(class_1);
+mu_2 = mean(class_2);
+centers_1 = centers(thresh_labels==1,:);
+centers_2 = centers(thresh_labels==2,:);
 
+class_image = zeros(size(label_map));
+
+for i=1:size(mask,3)
+   coin_mask = mask(:,:,i);
+   coin_mask_labeled = coin_mask*(classification(i)+1);
+   class_image = class_image+coin_mask_labeled;
+end
+
+if mu_1 > mu_2
+    class1_name=sprintf('pennies');
+    class2_name=sprintf('dimes');
+else
+    class1_name=sprintf('dimes');
+    class2_name=sprintf('pennies');
+end
+
+shaded_label_img = labeloverlay(I,class_image);
+figure(9), imshow(I,[]);  
+viscircles(centers_1, class_1,'Color','b'); 
+viscircles(centers_2, class_2,'Color', 'r'); 
+title('Class Labeled Image');
+text(10,30, class1_name, 'Color', 'b', 'FontSize', 15, 'FontWeight', 'bold');
+text(10,80, class2_name, 'Color', 'r', 'FontSize', 15, 'FontWeight', 'bold');
