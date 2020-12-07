15daee832ccd64bc8422939ef662c8b88f9695df
abpwrs <alexander-powers@uiowa.edu>
2020-12-07 16:01:05 -0600

    switching to 2 stage kmeans

--- src/part2.m
+++ src/part2.m
@@ -4,7 +4,7 @@
 
 %% Read in Coin Images
 src_dir = pwd();
-fname = '2-simple_res-05.jpg';
+fname = '1-simple_res-05.jpg';
 filesep_idx = strfind(src_dir, filesep);
 data_folder = strcat(src_dir(1:filesep_idx(end)), 'data/');
 I = imread(strcat(data_folder, fname));
@@ -12,7 +12,7 @@ is_complex = logical(numel(strfind(fname, 'complex')));
 
 %% Remove Noisy Background (Coin Mask Generation)
 if is_complex
-    coin_mask = generateCoinMaskComplex(I);
+    coin_mask = removeNoisyBackground(I);
 else
     coin_mask = generateCoinMaskSimple(I);
 end
@@ -25,7 +25,7 @@ end
 [centers, radii, metric] = imfindcircles(coin_mask, [min_radius max_radius], 'ObjectPolarity','bright', 'Sensitivity', 0.9);
 
 % Display Detected Circles
-figure(2);imshow(I,[]);
+figure(1);imshow(I,[]);
 viscircles(centers, radii, 'EdgeColor','b');
 
 %% Label Map and Channel-wise Mask Generation
@@ -34,38 +34,96 @@ label_map = generateLabelMap(mask, radii, coin_mask);
 
 %% Segmentation Label Overlay
 overlay = labeloverlay(I, label_map);
-figure(3); imshow(overlay,[]);
-figure(4); imshow(label_map,[]);
+figure(2); imshow(overlay,[]);
+figure(3); imshow(label_map,[]);
 
 % more visualizations of segmentation
 BW_coin_mask = label_map > 0;
-figure(5);imshow(BW_coin_mask,[]);
+figure(4);imshow(BW_coin_mask,[]);
 
-%% All in one 7D kmeans 
-k = 3; % 3 --> part2
-class_labels = cluster_coins(I, mask, radii, k);
+%% radii based classification using 1D kmeans
+k = 2; % 3 --> part2
+radii_based_labels =  kmeans(radii, k);
+mu_1 = mean(radii(radii_based_labels==1));
+mu_2 = mean(radii(radii_based_labels==2));
+if mu_1 > mu_2
+    quarter_radii = radii(radii_based_labels==1,:);
+    quarter_centers = centers(radii_based_labels==1,:);
+    quarter_mu = mu_1;
+    other_radii = radii(radii_based_labels==2,:);
+    other_centers = centers(radii_based_labels==2,:);
+    
+    other_mask = mask(:,:,radii_based_labels==2);
+else
+    quarter_radii = radii(radii_based_labels==2,:);
+    quarter_centers = centers(radii_based_labels==2,:);
+    quarter_mu = mu_1;
+    other_radii = radii(radii_based_labels==1,:);
+    other_centers = centers(radii_based_labels==1,:);
+    other_mask = mask(:,:,radii_based_labels==1);
 
-class_1 = radii(class_labels==1);
-class_2 = radii(class_labels==2);
-class_3 = radii(class_labels==3);
+end
 
+
+quarter_name=sprintf('Quarters');
+other_name=sprintf('Other');
+
+figure(5); imshow(I,[]);  
+viscircles(quarter_centers, quarter_radii,'Color','b'); 
+viscircles(other_centers, other_radii,'Color', 'r'); 
+title('Class Labeled Image');
+text(10,30, quarter_name, 'Color', 'b', 'FontSize', 15, 'FontWeight', 'bold');
+text(10,80, other_name, 'Color', 'r', 'FontSize', 15, 'FontWeight', 'bold');
+
+
+
+
+%% generate 7D colour + radii clustering features
+
+I_hsv = rgb2hsv(I);
+n_coins = size(other_mask,3);
+features = ones(n_coins,7);
+features(:,7) = other_radii;
+for coin_idx=1:n_coins
+    % rgb features
+    color_coin_mask = I .* uint8(other_mask(:,:,coin_idx));
+    for channel_idx=1:3
+        channel = color_coin_mask(:,:,channel_idx);
+        channel_sum = sum(channel, 'all');
+        channel_elem_count = sum(uint8(other_mask(:,:,coin_idx)),'all');
+        features(coin_idx, channel_idx) = (channel_sum / channel_elem_count)/256;
+    end
+    
+    % hsv features
+    color_coin_mask = I_hsv .* double(other_mask(:,:,coin_idx));
+    offset = 3;
+    for channel_idx=1:3
+        channel = color_coin_mask(:,:,channel_idx);
+        channel_sum = sum(channel, 'all');
+        channel_elem_count = sum(uint8(other_mask(:,:,coin_idx)),'all');
+        features(coin_idx, channel_idx+offset) = (channel_sum / channel_elem_count);
+    end
+end
+%% color based classification using ?-D kmeans
+k = 2; % 3 --> part2
+radii_based_labels =  kmeans(features, k);
+class_1 = other_radii(radii_based_labels==1);
+class_2 = other_radii(radii_based_labels==2);
 mu_1 = mean(class_1);
 mu_2 = mean(class_2);
-mu_3 = mean(class_3);
+centers_1 = other_centers(radii_based_labels==1,:);
+centers_2 = other_centers(radii_based_labels==2,:);
+class_image = zeros(size(label_map));
+class1_name = get_class_name_mu1_k3(mu_1,mu_2, quarter_mu);
+class2_name = get_class_name_mu1_k3(mu_2,quarter_mu, mu_1);
+class3_name = get_class_name_mu1_k3(quarter_mu,mu_1, mu_2);
 
-centers_1 = centers(class_labels==1,:);
-centers_2 = centers(class_labels==2,:);
-centers_3 = centers(class_labels==3,:);
 
-class1_name = get_class_name_mu1_k3(mu_1,mu_2,mu_3);
-class2_name = get_class_name_mu1_k3(mu_2,mu_3,mu_1);
-class3_name = get_class_name_mu1_k3(mu_3,mu_1, mu_2);
 
-figure(10); imshow(I,[]);  
+figure(6); imshow(I,[]);  
 viscircles(centers_1, class_1,'Color','b'); 
 viscircles(centers_2, class_2,'Color', 'r'); 
-viscircles(centers_3, class_3,'Color', 'g'); 
-
+viscircles(quarter_centers, quarter_radii,'Color', 'g'); 
 title('Class Labeled Image');
 text(10,30, class1_name, 'Color', 'b', 'FontSize', 15, 'FontWeight', 'bold');
 text(10,80, class2_name, 'Color', 'r', 'FontSize', 15, 'FontWeight', 'bold');
--- src/tmp_part2_func.m
+++ src/tmp_part2_func.m
@@ -15,7 +15,7 @@ function [done] = tmp_part2_func(file_obj, data_folder, figure_dir)
     
     f100 = figure(100);
     imshow(coin_mask);
-    saveas(f100, strcat(figure_dir, 'f100.png'));
+    saveas(f100, strcat(figure_dir, 'coin_mask.png'));
 
     % Hough Transform
     [min_radius, max_radius] = findRadiusBounds(coin_mask);
@@ -26,7 +26,7 @@ function [done] = tmp_part2_func(file_obj, data_folder, figure_dir)
     % Display Detected Circles
     f2 = figure(2);imshow(I,[]);
     viscircles(centers, radii, 'EdgeColor','b');
-    saveas(f2, strcat(figure_dir, 'f2.png'));
+    saveas(f2, strcat(figure_dir, 'circles.png'));
 
     % Label Map and Channel-wise Mask Generation
     mask = generateChannelMask(centers, radii, coin_mask);
@@ -35,46 +35,120 @@ function [done] = tmp_part2_func(file_obj, data_folder, figure_dir)
     % Segmentation Label Overlay
     overlay = labeloverlay(I, label_map);
     f3 = figure(3); imshow(overlay,[]);
-    saveas(f3, strcat(figure_dir, 'f3.png'));
+    saveas(f3, strcat(figure_dir, 'label_overlay.png'));
 
     f4 = figure(4); imshow(label_map,[]);
-    saveas(f4, strcat(figure_dir, 'f4.png'));
+    saveas(f4, strcat(figure_dir, 'label_map.png'));
 
     % more visualizations of segmentation
     BW_coin_mask = label_map > 0;
     f5 = figure(5);imshow(BW_coin_mask,[]);
-    saveas(f5, strcat(figure_dir, 'f5.png'));
+    saveas(f5, strcat(figure_dir, 'clean_coin_mask.png'));
 
-    % All in one 7D kmeans 
-    k = 3; % 3 --> part2
-    class_labels = cluster_coins(I, mask, radii, k);
+    
+    
+    % radii based classification using 1D kmeans
+    k = 2; % 3 --> part2
+    radii_based_labels =  kmeans(radii, k);
+    mu_1 = mean(radii(radii_based_labels==1));
+    mu_2 = mean(radii(radii_based_labels==2));
+    if mu_1 > mu_2
+        quarter_radii = radii(radii_based_labels==1,:);
+        quarter_centers = centers(radii_based_labels==1,:);
+        quarter_mu = mu_1;
+        other_radii = radii(radii_based_labels==2,:);
+        other_centers = centers(radii_based_labels==2,:);
 
-    class_1 = radii(class_labels==1);
-    class_2 = radii(class_labels==2);
-    class_3 = radii(class_labels==3);
+        other_mask = mask(:,:,radii_based_labels==2);
+    else
+        quarter_radii = radii(radii_based_labels==2,:);
+        quarter_centers = centers(radii_based_labels==2,:);
+        quarter_mu = mu_1;
+        other_radii = radii(radii_based_labels==1,:);
+        other_centers = centers(radii_based_labels==1,:);
+        other_mask = mask(:,:,radii_based_labels==1);
 
-    mu_1 = mean(class_1);
-    mu_2 = mean(class_2);
-    mu_3 = mean(class_3);
+    end
 
-    centers_1 = centers(class_labels==1,:);
-    centers_2 = centers(class_labels==2,:);
-    centers_3 = centers(class_labels==3,:);
 
-    class1_name = get_class_name_mu1_k3(mu_1,mu_2,mu_3);
-    class2_name = get_class_name_mu1_k3(mu_2,mu_3,mu_1);
-    class3_name = get_class_name_mu1_k3(mu_3,mu_1, mu_2);
+    quarter_name=sprintf('Quarters');
+    other_name=sprintf('Other');
 
     f6 = figure(6); imshow(I,[]);  
-    viscircles(centers_1, class_1,'Color','b'); 
-    viscircles(centers_2, class_2,'Color', 'r'); 
-    viscircles(centers_3, class_3,'Color', 'g'); 
-
+    viscircles(quarter_centers, quarter_radii,'Color','b'); 
+    viscircles(other_centers, other_radii,'Color', 'r'); 
     title('Class Labeled Image');
-    text(10,30, class1_name, 'Color', 'b', 'FontSize', 15, 'FontWeight', 'bold');
-    text(10,80, class2_name, 'Color', 'r', 'FontSize', 15, 'FontWeight', 'bold');
+    text(10,30, quarter_name, 'Color', 'b', 'FontSize', 15, 'FontWeight', 'bold');
+    text(10,80, other_name, 'Color', 'r', 'FontSize', 15, 'FontWeight', 'bold');
+    saveas(f6, strcat(figure_dir, 'kmeans_stage1.png'));
+
+
+
+
+    % generate 7D colour + radii clustering features
+    I_hsv = rgb2hsv(I);
+    n_coins = size(other_mask,3);
+    features = ones(n_coins,7);
+    features(:,7) = other_radii;
+    for coin_idx=1:n_coins
+        % rgb features
+        color_coin_mask = I .* uint8(other_mask(:,:,coin_idx));
+        for channel_idx=1:3
+            channel = color_coin_mask(:,:,channel_idx);
+            channel_sum = sum(channel, 'all');
+            channel_elem_count = sum(uint8(other_mask(:,:,coin_idx)),'all');
+            features(coin_idx, channel_idx) = (channel_sum / channel_elem_count)/256;
+        end
+
+        % hsv features
+        color_coin_mask = I_hsv .* double(other_mask(:,:,coin_idx));
+        offset = 3;
+        for channel_idx=1:3
+            channel = color_coin_mask(:,:,channel_idx);
+            channel_sum = sum(channel, 'all');
+            channel_elem_count = sum(uint8(other_mask(:,:,coin_idx)),'all');
+            features(coin_idx, channel_idx+offset) = (channel_sum / channel_elem_count);
+        end
+    end
+    % color based classification using 7D kmeans
+    k = 2; % 3 --> part2
+    radii_based_labels =  kmeans(features, k);
+    
+    class_1 = other_radii(radii_based_labels==1);
+    class_2 = other_radii(radii_based_labels==2);
+    
+    mu_1 = mean(class_1);
+    mu_2 = mean(class_2);
+    
+    centers_1 = other_centers(radii_based_labels==1,:);
+    centers_2 = other_centers(radii_based_labels==2,:);
+    
+    if mu_1 > mu_2
+        class1_name = sprintf("pennies");
+        class1_color = 'b';
+        class2_name = sprintf("dimes");
+        class2_color = 'r';
+    else
+        class1_name = sprintf("dimes");
+        class1_color = 'r';
+        class2_name = sprintf("pennies");
+        class2_color = 'b';
+    end
+    class3_name = sprintf("quarters");
+
+
+
+    f7 = figure(7); imshow(I,[]);  
+    viscircles(centers_1, class_1,'Color', class1_color); 
+    viscircles(centers_2, class_2,'Color', class2_color); 
+    viscircles(quarter_centers, quarter_radii,'Color', 'g'); 
+    title('Class Labeled Image');
+    text(10,30, class1_name, 'Color', class1_color, 'FontSize', 15, 'FontWeight', 'bold');
+    text(10,80, class2_name, 'Color', class2_color, 'FontSize', 15, 'FontWeight', 'bold');
     text(10,130, class3_name, 'Color', 'g', 'FontSize', 15, 'FontWeight', 'bold');
-    saveas(f6, strcat(figure_dir, 'f6.png'));
+    
+    saveas(f7, strcat(figure_dir, 'kmeans_stage2.png'));
+
     done = 1;
 end
 
